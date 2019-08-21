f947969311b33263f30fef934877a7478908f106
ariyadiaip <ariyadiaip@gmail.com>
2019-08-21 14:23:26 +0700

    Initial commit

---
+++ app/src/main/java/id/smkn4/pwpb/bottomnavfragment/MainActivity.java
@@ -0,0 +1,53 @@
+package id.smkn4.pwpb.bottomnavfragment;
+
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new DashboardFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new NotificationsFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+    private boolean loadFragment(Fragment frag) {
+        if (frag != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, frag).commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/id/smkn4/pwpb/bottomnavfragment/ProfileFragment.java
@@ -0,0 +1,18 @@
+package id.smkn4.pwpb.bottomnavfragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, null);
+    }
+}
